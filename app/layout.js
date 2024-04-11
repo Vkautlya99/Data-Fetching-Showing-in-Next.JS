3ef0b59d3564a08433434269314263359b368acb
Vikram kautlya <89295700+Vkautlya99@users.noreply.github.com>
2024-04-11 12:21:16 +0530

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/Components/Header";
+// import Header from "@/Components/Header";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -12,7 +12,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Header/>
         {children}</body>
     </html>
   );
